ed1c822f906f7fe091fa31a1f1e3920e336a7a76
Uditansh Srivastava <157925959+UditanshSri@users.noreply.github.com>
2024-04-11 19:31:13 +0530

    Update streamlitapi.py

--- streamlitapi.py
+++ streamlitapi.py
@@ -44,7 +44,6 @@ with col5:
 if st.button('Predict Probability'):
     runs_left = target - score
     balls_left = 120 - (overs * 6)
-    wickets = 10 - wickets
     crr = score / overs
     rrr = (runs_left * 6) / balls_left
 
