e4d06138b13e9ffd55d99b51e9c8e016bea44f2e
Uditansh Srivastava <157925959+UditanshSri@users.noreply.github.com>
2024-04-12 03:36:53 +0530

    Update streamlitapi.py

--- streamlitapi.py
+++ streamlitapi.py
@@ -16,21 +16,19 @@ teams = [
 ]
 
 cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
-          'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
-          'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
-          'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
+          'Chandigarh', 'Jaipur', 'Chennai','Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
           'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
           'Sharjah', 'Mohali', 'Bengaluru']
 
 col1, col2 = st.columns(2)
 with col1:
-    batting_team = st.selectbox('Select the batting team', sorted(teams))
+    batting_team = st.selectbox('Select the batting team', teams)
 with col2:
-    bowling_team = st.selectbox('Select the bowling team', sorted(teams))
+    bowling_team = st.selectbox('Select the bowling team', teams)
 
 target = st.number_input('Target Score')
 
-selected_city = st.selectbox('Select host city', sorted(cities))
+selected_city = st.selectbox('Select host city', cities)
 
 col3, col4, col5 = st.columns(3)
 with col3:
